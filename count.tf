5619ebe840c17c28dfa37d1991e36dbb03384ce5
Micah Shallom <micahshallom@gmail.com>
2022-10-29 15:36:25 +0100

    splat

--- count.tf
+++ count.tf
@@ -1,39 +1,40 @@
-terraform {
-  required_providers {
-    docker = {
-      source  = "kreuzwerker/docker"
-      version = "2.22.0"
-    }
-  }
-}
+# terraform {
+#   required_providers {
+#     docker = {
+#       source  = "kreuzwerker/docker"
+#       version = "2.22.0"
+#     }
+#   }
+# }
 
-provider "docker" {}
+# provider "docker" {}
 
-resource "docker_image" "nginx_image" {
-  name = "nginx:1.23.2-alpine"
-}
+# resource "docker_image" "nginx_image" {
+#   name = "nginx:1.23.2-alpine"
+# }
 
-resource "random_string" "random" {
-    count            = 2
-    length           = 16
-    special          = false
-    upper            = false
-}
+# resource "random_string" "random" {
+#     count            = 2
+#     length           = 16
+#     special          = false
+#     upper            = false
+# }
 
-resource "docker_container" "nginx_container"{
-    count = 2
-    name = join("-",["nginx", random_string.random[count.index].result])
-    image = docker_image.nginx_image.latest
-    ports {
-      internal = 80
-      # external = 8000
-    }
-}
+# resource "docker_container" "nginx_container"{
+#     count = 2
+#     name = join("-",["nginx", random_string.random[count.index].result])
+#     image = docker_image.nginx_image.latest
+#     ports {
+#       internal = 80
+#       # external = 8000
+#     }
+# }
 
 
-output "nginx-url1" {
-    value = join(":", [docker_container.nginx_container[0].ip_address, docker_container.nginx_container[0].ports[0].external])
-}
-output "nginx-url2" {
-    value = join(":", [docker_container.nginx_container[1].ip_address, docker_container.nginx_container[1].ports[0].external])
-}
+# output "nginx-url1" {
+#     value = join(":", [docker_container.nginx_container[0].ip_address, docker_container.nginx_container[0].ports[0].external])
+# }
+
+# output "nginx-url2" {
+#     value = join(":", [docker_container.nginx_container[1].ip_address, docker_container.nginx_container[1].ports[0].external])
+# }
---
+++ splat.tf
@@ -0,0 +1,45 @@
+### splat allows one to target multiple resource in a resource group and display its output
+
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "2.22.0"
+    }
+  }
+}
+
+provider "docker" {}
+
+resource "docker_image" "nginx_image" {
+  name = "nginx:1.23.2-alpine"
+}
+
+resource "random_string" "random" {
+    count            = 2
+    length           = 16
+    special          = false
+    upper            = false
+}
+
+resource "docker_container" "nginx_container"{
+    count = 2
+    name = join("-",["nginx", random_string.random[count.index].result])
+    image = docker_image.nginx_image.latest
+    ports {
+      internal = 80
+      # external = 8000
+    }
+}
+
+output "nginx_container_names" {
+    value = docker_container.nginx_container[*].name
+
+}
+output "nginx-url1" {
+    value = join(":", [docker_container.nginx_container[0].ip_address, docker_container.nginx_container[0].ports[0].external])
+}
+
+output "nginx-url2" {
+    value = join(":", [docker_container.nginx_container[1].ip_address, docker_container.nginx_container[1].ports[0].external])
+}
