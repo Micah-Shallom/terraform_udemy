699d7712eaac37eaf368d682480b662beef223fc
Micah Shallom <micahshallom@gmail.com>
2022-10-29 15:12:07 +0100

    count.tf

--- count.tf
+++ count.tf
@@ -13,14 +13,16 @@ resource "docker_image" "nginx_image" {
   name = "nginx:1.23.2-alpine"
 }
 
-resource "random_string" "random1" {
+resource "random_string" "random" {
+    count            = 2
     length           = 16
     special          = false
-    upper = false
+    upper            = false
 }
 
-resource "docker_container" "nginx_container1"{
-    name = join("-",["nginx", random_string.random1.result])
+resource "docker_container" "nginx_container"{
+    count = 2
+    name = join("-",["nginx", random_string.random[count.index].result])
     image = docker_image.nginx_image.latest
     ports {
       internal = 80
@@ -29,9 +31,9 @@ resource "docker_container" "nginx_container1"{
 }
 
 
-# output "nginx-url1" {
-#     value = join(":", [docker_container.nginx_container1.ip_address, docker_container.nginx_container1.ports[0].external])
-# }
-# output "nginx-url2" {
-#     value = join(":", [docker_container.nginx_container2.ip_address, docker_container.nginx_container2.ports[0].external])
-# }
+output "nginx-url1" {
+    value = join(":", [docker_container.nginx_container[0].ip_address, docker_container.nginx_container[0].ports[0].external])
+}
+output "nginx-url2" {
+    value = join(":", [docker_container.nginx_container[1].ip_address, docker_container.nginx_container[1].ports[0].external])
+}
