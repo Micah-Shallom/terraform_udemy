97b3f8738492b022991bffd2c0a4655e4add2e98
Micah Shallom <micahshallom@gmail.com>
2022-10-31 13:51:58 +0100

    using variables and setting up variable conditionals for validating input

--- import.tf
+++ import.tf
@@ -1,39 +1,42 @@
-### splat allows one to target multiple resource in a resource group and display its output
-provider "docker" {}
+# ### splat allows one to target multiple resource in a resource group and display its output
+# provider "docker" {}
 
-resource "docker_image" "nginx_image" {
-  name = "nginx:1.23.2-alpine"
-}
+# resource "docker_image" "nginx_image" {
+#   name = "nginx:1.23.2-alpine"
+# }
 
-resource "random_string" "random" {
-    count       = 2
-    length      = 4
-    special     = false
-    upper       = false
-}
+# resource "random_string" "random" {
+#     count       = 2
+#     length      = 4
+#     special     = false
+#     upper       = false
+# }
 
-resource "docker_container" "nginx_container" {
-    count       = 2
-    name        = join("-", ["nginx", random_string.random[count.index].result])
-    image       = docker_image.nginx_image.latest
-    ports        {
-        internal        = 80
-        # external      = 8000
-    }
-}
+# resource "docker_container" "nginx_container" {
+#     count       = 2
+#     name        = join("-", ["nginx", random_string.random[count.index].result])
+#     image       = docker_image.nginx_image.latest
+#     ports        {
+#         internal        = 80
+#         # external      = 8000
+#     }
+# }
 
-resource "docker_container" "nginx_container_alien" {
-    name = "gracious_mahavira"
-    image = docker_image.nginx_image.latest
-}
+# resource "docker_container" "nginx_container_alien" {
+#     name = "gracious_mahavira"
+#     image = docker_image.nginx_image.latest
+# }
 
-output "nginx_container_names" {
-    value = [for i in docker_container.nginx_container[*] : join(":", [i.ip_address], i.ports[*]["external"])]
-}
+# output "nginx_container_names" {
+#     value = [for i in docker_container.nginx_container[*] : join(":", [i.ip_address], i.ports[*]["external"])]
+# }
 
-# output
-###################################################################################
-# nginx_container_names = [
-#   "172.17.0.5:49158",
-#   "172.17.0.4:49157",
-# ]
+# # output
+# ###################################################################################
+# # nginx_container_names = [
+# #   "172.17.0.5:49158",
+# #   "172.17.0.4:49157",
+# # ]
+
+
+# # terraform import docker_container.nginx_container_alien $(docker inspect --format="{{.ID}}" gracious_mahavira)
