dd900902b284b68866c5ed009be927f26621b45c
Micah Shallom <micahshallom@gmail.com>
2022-11-07 15:09:13 +0100

    using locals to add functions to values

--- variables.tf
+++ variables.tf
@@ -1,11 +1,10 @@
 variable "ext_port" {
-    type = number
-    default = 8000
+    type = list
 
-    validation  {
-        condition = var.ext_port > 0  && var.ext_port <= 65535
-        error_message = "The external port must fall between the range of 0 - 65535."
-    }
+    # validation  {
+    #     condition = var.ext_port > 0  && var.ext_port <= 65535
+    #     error_message = "The external port must fall between the range of 0 - 65535."
+    # }
 }
 variable "int_port" {
     type = number
@@ -16,7 +15,11 @@ variable "int_port" {
         error_message = "The internal port must be port 80."
     }
 }
-variable "container_count" {
-    type = number
-    default = 2
+# variable "container_count" {
+#     type = number
+#     default = 2
+# }
+
+locals {
+  container_count = length(var.ext_port)
 }
